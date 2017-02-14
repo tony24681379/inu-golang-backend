dbfb696931424f9fea210975f8dd09e57da0c3af
Tony Huang <yellow24681379@gmail.com>
2017-02-14 10:59:51 +0800

    test esclient

--- server/server.go
+++ server/server.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/tony24681379/inu-golang-backend/esclient"
+	elastic "gopkg.in/olivere/elastic.v5"
 )
 
 type amiast struct {
@@ -15,10 +17,10 @@ type amiast struct {
 }
 
 //Server init
-func Server(elasticSearchIP, elasticSearchPort string) {
+func Server2(elasticSearchIP, elasticSearchPort string) {
 	ec, err := esclient.CreateESClient(elasticSearchIP, elasticSearchPort)
 	if err != nil {
-		print(err)
+		logrus.Error("Can not connect to ElasticSearch")
 	}
 	// Search Using Raw json String
 	// searchJSON := `{
@@ -63,4 +65,44 @@ func Server(elasticSearchIP, elasticSearchPort string) {
 
 	//	ch := make(chan int)
 	//	<-ch
+}
+
+//Server init
+func Server(elasticSearchIP, elasticSearchPort string) {
+	client, err := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL("http://"+elasticSearchIP+":"+elasticSearchPort))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	/*	esversion, err := client.ElasticsearchVersion("http://" + elasticSearchIP + ":" + elasticSearchPort)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Elasticsearch version %s", esversion)
+	*/
+	// Ping the Elasticsearch server to get e.g. the version number
+	info, code, err := client.Ping("http://" + elasticSearchIP + ":" + elasticSearchPort).Do(context.Background())
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
+
+	get1, err := client.Get().
+		Index("logs-2016.05.11").
+		Type("amiast").
+		Id("B95F18EFB979D565AB84-A71D30C0210399FB-ED6A41971D1-A2EDE7FD00000CF31FC00AD7").
+		Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	} else if get1.Found {
+		fmt.Printf("Got document %s in version %d from index %s, type %s\n", get1.Id, get1.Version, get1.Index, get1.Type)
+		o, err := get1.Source.MarshalJSON()
+		if err != nil {
+			logrus.Error("error to get base")
+		}
+		var vtt amiast
+		json.Unmarshal(o, &vtt)
+		fmt.Print(vtt)
+	}
 }
